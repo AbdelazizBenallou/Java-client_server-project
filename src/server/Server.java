50915079a8bd961075906f3df7b8d96110d248b2
root <vivovivo217712@protonmail.com>
2025-11-09 17:14:11 -0600

    VersionTwo

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,13 +1,17 @@
 package server;
+import java.io.*;
 import java.net.*;
 import java.sql.*;
 
 public class Server {
-
-    public static void main(String[] args) {
+    
+    private static BufferedReader reader;
+    private static BufferedWriter writer;
+    
+    public static void main(String[] args) throws IOException {
         ServerSocket server = null;
         Socket socket = null;
-        int port = 8000;
+        int port = 8020;
         String address = "127.0.0.1";
         String db_name = Var_Env.DB_NAME;
         String db_pass = Var_Env.DB_PASSWORD;
@@ -17,24 +21,68 @@ public class Server {
         String url = "jdbc:mysql://localhost:" + db_port + "/" + db_name;
         
         try{
+            //Create Server Socket and bind The Server 
             server = new ServerSocket();
             server.bind(
               new InetSocketAddress(address, port)
             );
             System.out.println("Server bind : "+ address + ":"+port);
-            
+           
+            //Create Connection with database
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(url, db_user, db_pass);
-            
-            System.out.println("Connection Success !");
-            
-            while(true){
+           
                 socket = server.accept();
-                System.out.println("Client Connected Success !");
-            }
+                
+                 //Create objects for send and recieve Messeges
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                
+                sendMessage("Hello , Please Login");
+                
+                sendMessage("Enter Your Email: ");
+                String email = receiveMessage();
+                System.out.println(email);
+                
+                sendMessage("Enter Your Password:");
+                String password = receiveMessage();
+                System.out.println(password);
+                
+                //Check Email Exist 
+                String email_check =  "SELECT * FROM users WHERE email = ? AND password = ?";
+                PreparedStatement stmt = conn.prepareStatement(email_check);
+                stmt.setString(1, email);
+                stmt.setString(2, password);
+                ResultSet rs = stmt.executeQuery();
+                
+                
+                if (rs.next()){
+                     int userId = rs.getInt("id");
+                     sendMessage("Login successful! " + userId);
+                     
+                }else{
+                    sendMessage("Invalid email or password!");
+                }
+                
+               
         }catch(Exception e){
             System.out.println(e);
+        }finally{
+            //Close Ressources
+            
+            if (server != null) server.close();
+            if (socket != null) socket.close();
+            if (reader != null) reader.close();
+            if (writer != null) writer.close();
         }
     }
     
+    public static void sendMessage(String message) throws IOException {
+        writer.write(message + "\n");
+        writer.flush();
+    }
+    
+    public static String receiveMessage() throws IOException {
+        return reader.readLine();
+    }
 }
